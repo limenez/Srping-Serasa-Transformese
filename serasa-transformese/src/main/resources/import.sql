8b71f004f0f338e6eca334d2029b27f1f7ed3b6d
Livia Menezes <83612708+limenez@users.noreply.github.com>
2023-06-08 20:15:14 -0300

    Update import.sql

--- serasa-transformese/src/main/resources/import.sql
+++ serasa-transformese/src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into clinica (id, nome) values (1, 'Bem estar');
 insert into clinica (id, nome) values (2, 'Saúde mais');
+insert into clinica (id, nome) values (3, 'Clinica Livia');
+insert into clinica (id, nome) values (4, 'Fit Academia');
+insert into clinica (id, nome) values (5, 'Psicologia');
+insert into clinica (id, nome) values (6, 'Terapias Ocupacionais');
